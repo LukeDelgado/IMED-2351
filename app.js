3dcab1f203e429f7a8a297eb4c06c0180924e468
LukeDelgado <156985469+LukeDelgado@users.noreply.github.com>
2025-02-01 00:36:23 -0600

    Assignment Variables

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-console.log("What's your favorite season?");
 
-document.write("<h1>What's your favorite holiday?</h1>");
+var startingtime = (123);  // Global Variable in Secondss
+
+const stopwatch = {color:"#FFFFFF", font:"SegoeUI Bold"};  // Object Defined
+
+const timetotal = ("Total Time");  // String Variable
+
+console.log(timetotal);  // Console Output String
+
+{
+    
+    let divisor = (startingtime)  // Block Variable
+    
+    divisor = divisor / 60;  // Math Operation
+    
+    console.log(divisor);  // Console Output Number
+    
+}
+
+
+
+
+
