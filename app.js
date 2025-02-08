640a7f2b292f790d87cd7ff030920b1bff3e389b
LukeDelgado <156985469+LukeDelgado@users.noreply.github.com>
2025-02-08 03:54:49 -0600

    3.5

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-
 const stopwatch = {color:"#FFFFFF", font:"SegoeUI Bold"};  // Object Defined
 
+const txt1 = document.getElementById("inputSeconds");
+const btn1 = document.getElementById("SubmitButton");
+const out1 = document.getElementById("outputTime");
 
-function divider(startingtime){
-    let divisor = (startingtime)  // Block Variable
-    divisor = divisor / 60;  // Math Operation
-    console.log(divisor);  // Console Output Number
+function convertToMinutes(startingtime){
+    let divisor = (startingtime / 60).toFixed(2);
+    console.log("Total time: " + divisor);
+    out1.innerHTML=divisor
 }
 
-function timeTotal(){
-    const timeTotal = ("Total Time");  // String Variable
-    console.log(timeTotal);  // Console Output String
+
+function buttonSubmit(){
+    convertToMinutes(txt1.value);
 }
-
-timeTotal();
-divider(255);
-
-
-
-
