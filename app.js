94fa11bbc8c02095130b5526a112bd115068376d
LukeDelgado <156985469+LukeDelgado@users.noreply.github.com>
2025-02-08 00:27:43 -0600

    Update

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 
-var startingtime = (123);  // Global Variable in Secondss
-
 const stopwatch = {color:"#FFFFFF", font:"SegoeUI Bold"};  // Object Defined
 
-const timetotal = ("Total Time");  // String Variable
 
-console.log(timetotal);  // Console Output String
-
-{
-    
+function divider(startingtime){
     let divisor = (startingtime)  // Block Variable
-    
     divisor = divisor / 60;  // Math Operation
-    
     console.log(divisor);  // Console Output Number
-    
 }
 
+function timeTotal(){
+    const timeTotal = ("Total Time");  // String Variable
+    console.log(timeTotal);  // Console Output String
+}
+
+timeTotal();
+divider(255);
 
 
 
